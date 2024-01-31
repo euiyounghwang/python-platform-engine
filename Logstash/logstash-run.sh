b295c0464434df371f67dc7753e57596626614c9
euiyounghwang <marieuig@gmail.com>
2024-01-30 23:05:15 -0600

    Update

--- Logstash/logstash-run.sh
+++ Logstash/logstash-run.sh
@@ -5,6 +5,7 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # echo $SCRIPTDIR
 # echo $PWD
+
 export LOGSTASH_INTERNAL_PASSWORD=gsaadmin
 
 $SCRIPTDIR/logstash-7.9.0/bin/logstash -f $SCRIPTDIR/logstash_udp.conf
