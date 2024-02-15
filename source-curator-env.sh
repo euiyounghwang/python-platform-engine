2033e69cc8ef91b0c70c50ca05454bf1487c097e
euiyounghwang <marieuig@gmail.com>
2024-02-14 21:17:05 -0600

    Update elasticsearch scripts

--- source-curator-env.sh
+++ source-curator-env.sh
@@ -5,6 +5,12 @@ export PYTHONDONTWRITEBYTECODE=1
 
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-# --
-# source ./source-curator-env.sh
-source $SCRIPTDIR/Monitoring/Curator/.venv/bin/activate
+if [ -d $SCRIPTDIR//Search-Engine/Docker/elasticsearch/$VENV ]; then
+    echo "VirtualEnv exists."
+    # --
+    # source ./source-curator-env.sh
+    source $SCRIPTDIR/Monitoring/Curator/.venv/bin/activate
+else
+    echo "VirtualEnv doesn't exists."
+    source $SCRIPTDIR/Monitoring/Curator/create_virtual_env.sh
+fi
