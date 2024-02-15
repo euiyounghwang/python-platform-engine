c0475127545a4055f0d83c2cd5c6d30bc8a504be
euiyounghwang <marieuig@gmail.com>
2024-02-14 21:10:39 -0600

    Update elasticsearch scripts

--- source-elasticsearch-env.sh
+++ source-elasticsearch-env.sh
@@ -6,5 +6,5 @@ export PYTHONDONTWRITEBYTECODE=1
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # --
-# source ./source-curator-env.sh
-source $SCRIPTDIR/Monitoring/Curator/.venv/bin/activate
+# source ./source-elasticsearch-env.sh
+source $SCRIPTDIR/Search-Engine/Docker/elasticsearch/.venv/bin/activate
