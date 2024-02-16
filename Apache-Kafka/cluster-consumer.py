618a33ed4f99e8bfb5f129e2c68cd2510d5b314b
euiyounghwang <marieuig@gmail.com>
2024-02-15 23:21:01 -0600

    Update Kafak

--- Apache-Kafka/cluster-consumer.py
+++ Apache-Kafka/cluster-consumer.py
@@ -1,6 +1,9 @@
 from kafka import KafkaConsumer
 from threading import Thread
 import time
+import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
 # poetry add kafka-python
@@ -8,13 +11,15 @@ def consumer_kafka(topic):
     brokers = ['localhost:29092', 'localhost:39092']
     # topic = 'test-topic'
     print('--', topic)
-    consumer = KafkaConsumer(topic, group_id="Python_Kafka_Consumer_Job", bootstrap_servers=brokers)
+    consumer = KafkaConsumer(topic, group_id="Python_Kafka_Consumer_App_Job", bootstrap_servers=brokers)
 
     for message in consumer:
-        print(message, message.value, message.value.decode("utf-8"))
+        logging.info(f'{message, message.value, message.value.decode("utf-8")}')
+    logging.info("-- job finished..--")
     
     
 def thread_background(topic):
+    
     while True:
         try:
             # print('create --', type(read_config_yaml()))
@@ -22,12 +27,13 @@ def thread_background(topic):
             consumer_kafka(topic)
         except Exception as e:
             # listen_kill_server()
+            print(e)
             pass
         time.sleep(5)  # TODO poll other things
         
         
         
 if __name__ == "__main__":
-    for topic in ['test-topic', 'test1-topic']:
+    for topic in ['test-topic','test1-topic']:
         # Create thread as background
         Thread(target=thread_background, args=(topic,)).start()
--- source-curator-create-index.sh
+++ source-curator-create-index.sh
@@ -6,11 +6,12 @@ export PYTHONDONTWRITEBYTECODE=1
 
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if [ -d $SCRIPTDIR//Search-Engine/Docker/elasticsearch/$VENV ]; then
+VENV=".venv"
+if [ -d $SCRIPTDIR/Search-Engine/Docker/elasticsearch/$VENV ]; then
     echo "VirtualEnv exists."
     # --
     # source ./source-curator-env.sh
-    source $SCRIPTDIR/Monitoring/Curator/.venv/bin/activate
+    source $SCRIPTDIR/Monitoring/Curator/$VENV/bin/activate
 else
     echo "VirtualEnv doesn't exists."
     source $SCRIPTDIR/Monitoring/Curator/create_virtual_env.sh
