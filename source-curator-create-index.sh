8afc308e939912b985412751c774d63a281ce077
euiyounghwang <marieuig@gmail.com>
2024-02-15 22:02:46 -0600

    Update

--- source-curator-create-index.sh
+++ source-curator-create-index.sh
@@ -3,7 +3,17 @@ set -e
 
 export PYTHONDONTWRITEBYTECODE=1
 
+
 SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-source $SCRIPTDIR/Monitoring/Curator/.venv/bin/activate
+if [ -d $SCRIPTDIR//Search-Engine/Docker/elasticsearch/$VENV ]; then
+    echo "VirtualEnv exists."
+    # --
+    # source ./source-curator-env.sh
+    source $SCRIPTDIR/Monitoring/Curator/.venv/bin/activate
+else
+    echo "VirtualEnv doesn't exists."
+    source $SCRIPTDIR/Monitoring/Curator/create_virtual_env.sh
+fi
+
 python $SCRIPTDIR/Monitoring/Curator/Create-index-data.py
