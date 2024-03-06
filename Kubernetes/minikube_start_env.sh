0e1646bce53effe24c4a4fe514e4c66c99e486f3
euiyounghwang <marieuig@gmail.com>
2024-03-05 22:17:13 -0600

    Update

--- Kubernetes/minikube_start_env.sh
+++ Kubernetes/minikube_start_env.sh
@@ -4,9 +4,3 @@ set -e
 
 minikube start
 minikube dashboard
-
-echo "build a docker image in our local machine and then deploy them to the minikube environment."
-eval $(minikube docker-env)
-
-echo "Running service in minikube cluser"
-minikube service fastapi-basic --url
