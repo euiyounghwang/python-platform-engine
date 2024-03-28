3d9cb969811bc6fefc08aff32eb8268a9d896cf1
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-03-28 14:02:20 -0500

    Update

--- Search-Engine/Shell_Scripts/SSH/ssh-connection.py
+++ Search-Engine/Shell_Scripts/SSH/ssh-connection.py
@@ -4,8 +4,10 @@ import logging
 
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-server_file = "./server"
-# server_file = "./server_test"
+# server_file = "C://Users//euiyoung.hwang/server"
+server_file = "C://Users//euiyoung.hwang/server_test"
+
+credentials_file = "C://Users//euiyoung.hwang/ssh_credentials"
 
 failed_server_list = []
 
@@ -19,14 +21,25 @@ def read_server():
     return server_list
 
 
-def ssh_connection_test(host):
+def read_credentials():
+    credentials = []
+    with open(credentials_file) as data_file:
+        for line in data_file:
+            line = line.strip().split(' ')
+            # print("test", line)
+            # logging.info(f"{line}")
+            credentials.extend(line)
+                
+    credentials = [item for item in credentials if len(item) > 0]
+    return credentials
+
+
+def ssh_connection_test(host, username, password):
     try:
         # command = "df"
 
         # Update the next three lines with your
         # server's information
-
-      
 
         client = paramiko.client.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -45,13 +58,16 @@ def ssh_connection_test(host):
 
 if __name__ == "__main__":
     server_list = read_server()
+    credential = read_credentials()
     # logging.info(server_list)
+    # print(server_list)
+    # print(credential)
 
     for idx, each_server in enumerate(server_list):
         # logging.info(f"{idx+1} : {each_server}")
         print(f"{idx+1} : {each_server}")
-        ssh_connection_test(str(each_server).strip())
-
+        ssh_connection_test(str(each_server).strip(), credential[0], credential[1])
+    
     # logging.info("\n")
     # logging.info(f"Failed server list : {failed_server_list}")
     # logging.info("\n")
