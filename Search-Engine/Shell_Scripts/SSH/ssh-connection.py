00263b73971cfb62838970d0588c02197db1ce95
Euiyoung Hwang <euiyoung.hwang@gxo.com>
2024-03-29 08:39:53 -0500

    Update ssh-connection

--- Search-Engine/Shell_Scripts/SSH/ssh-connection.py
+++ Search-Engine/Shell_Scripts/SSH/ssh-connection.py
@@ -2,10 +2,10 @@
 import paramiko
 import logging
 
-# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-server_file = "C://Users//euiyoung.hwang/server"
-# server_file = "C://Users//euiyoung.hwang/server_test"
+# server_file = "C://Users//euiyoung.hwang/server"
+server_file = "C://Users//euiyoung.hwang/server_test"
 
 credentials_file = "C://Users//euiyoung.hwang/ssh_credentials"
 
@@ -46,8 +46,7 @@ def ssh_connection_test(host, username, password):
         client.connect(host, username=username, password=password)
         # _stdin, _stdout,_stderr = client.exec_command("df")
         # print(_stdout.read().decode())
-        # logging.info(f"Success : {host}")
-        print(f"Success : {host}")
+        logging.info(f"Success : {host}")
     except Exception as error:
         # logging.error(f"Failed : {host}")
         print(f"Failed : {host}")
@@ -64,14 +63,9 @@ if __name__ == "__main__":
     # print(credential)
 
     for idx, each_server in enumerate(server_list):
-        # logging.info(f"{idx+1} : {each_server}")
-        print(f"{idx+1} : {each_server}")
+        logging.info(f"{idx+1} : {each_server}")
         ssh_connection_test(str(each_server).strip(), credential[0], credential[1])
     
-    # logging.info("\n")
-    # logging.info(f"Failed server list : {failed_server_list}")
-    # logging.info("\n")
-    
-    print("\n")
-    print(f"Failed server list (Total Count: {len(failed_server_list)}): {failed_server_list}")
-    print("\n")
+    logging.info("\n")
+    logging.info(f"Failed server list (Total Count: {len(failed_server_list)}): {failed_server_list}")
+    logging.info("\n")
